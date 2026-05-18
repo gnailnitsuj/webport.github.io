0fe1e290b1eb045964b95fe6fabfdc57ee94b433
Justin L <justinliang082@gmail.com>
2026-05-18 00:23:00 -0400

    Create serviceWorker.js

---
+++ serviceWorker.js
@@ -0,0 +1,18 @@
+const cacheName = 'bard-pwa-v1';
+const assets = [
+    '/pwa.html',
+    '/style.css',
+];
+
+self.addEventListener('install', async () => {
+    const cache = await caches.open(cacheName);
+    await cache.addAll(assets);
+});
+self.addEventListener('fetch', event => {
+    event.respondWith(cacheFirst(event.request));
+});
+async function cacheFirst(request) {
+    const cachedResponse = await caches.match(request);
+    return cachedResponse || fetch(request);
+}
+
