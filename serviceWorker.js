6a4a2039959fe5b3e031cc6a818fe1da7058408f
Justin L <justinliang082@gmail.com>
2026-05-22 00:41:43 -0400

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -2,6 +2,7 @@ const cacheName = 'bard-pwa-v1';
 const assets = [
     '/webport.github.io/pwa.html',
     '/webport.github.io/style.css',
+    '/webport.github.io/app.js'
 ];
 
 self.addEventListener('install', async () => {
